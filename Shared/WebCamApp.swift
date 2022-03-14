d362819ae1c44681bb8c031043e410fbd003a118
Nathan John <njhhome@outlook.com>
2022-03-14 02:04:02 +0000

    Initial Commit

---
+++ Shared/WebCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebCamApp.swift
+//  Shared
+//
+//  Created by Nathan John on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WebCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
